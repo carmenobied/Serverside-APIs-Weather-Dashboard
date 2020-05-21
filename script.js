57c1171653f03a11afe319fd6db9de4862dcb43d
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-05-21 13:20:11 -0700

    correctly displaying weather details for current city

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
 // When page is loaded, display currentDate on the page. Call moment.js api to display current date in real-time and future dates for 5-day forecast
 var currentDate = moment();
-$("#currentDate").text(moment().format('l'));
+// $("#currentDate").text(moment().format('l'));
 $("#0dayForecast").text(moment().add(1).format('l'));
 $("#1dayForecast").text(moment().add(2).format('l'));
 $("#2dayForecast").text(moment().add(3).format('l'));
@@ -51,27 +51,57 @@ function searchHistory() {
 }
 
 // Created apiKey 
-var apiKey = "1ff0f6823d723403dabe8415bdcb12e3";
-
-// Function getWeather
-function getWeather(city) {
-    console.log(city);
+var APIKey = "1ff0f6823d723403dabe8415bdcb12e3";
 
     // Build the URL needed to query the database of the OpenWeatherMap API
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appID=" + apiKey;
-    // LA example: http://api.openweathermap.org/data/2.5/forecast?q=Los+Angeles&appID=1ff0f6823d723403dabe8415bdcb12e3
+    // var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appID=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+     "q=Los+Angeles&appid=" + APIKey;
 
     // Run AJAX GET call to request the OpenWeatherMap API
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
+     url: queryURL,
+     method: "GET"
+   })
 
     // Store all of the retrieved data inside of a response object 
     .then(function(response) {
-    // log queryURL and resulting object
-    // console.log(queryURL);
+
+    // Log the queryURL
+    console.log(queryURL);
+    // Log the resulting object
     console.log(response);
+       
+    // Empties the divs to get rid of previous searches
+        $("#searchInput").empty();
+
+    // Display searched city name, temperature, humidity, wind speed, UV index (incl. weather icon) 
+    // Transfer content to HTML
+       $(".currentCity").html("<h1>" + response.name + "</h1>");
+    //    $("#currentCity").text(response.name + " (" + response.list[0].dt_txt.substr(0, 10) + ")").append(iconCode);
+       $(".windSpeed").text("Wind Speed: " + response.wind.speed);
+    //    $("#windSpeed").text("Wind Speed: " + response.list[6].wind.speed + " mph");
+       $(".humidity").text("Humidity: " + response.main.humidity);
+    //    $("#humidity").text("Humidity: " + response.list[6].main.humidity + " %");
+       
+    // Convert the temp to fahrenheit
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    // add temp content to html
+    $(".temperature").text("Temperature (K) " + response.main.temp);
+    // $("#temperature").text("Temperature: " + ((response.list[6].main.temp- 273.15) * 1.80 + 32).toFixed(2) + " F"); 
+       $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+       $(".uvIndex").text("UV Index: " + response.main.uvi);
+       var lat = response.city.coord.lat;
+       var long = response.city.coord.lon;
+       uvi(lat, long)  
+
+       // Log the data in the console as well
+       console.log("Current City: " + response.name);
+       console.log("Wind Speed: " + response.wind.speed);
+       console.log("Humidity: " + response.main.humidity);
+       console.log("Temperature (F): " + tempF);
+       console.log("UV Index: " + response.main.uvi);
 
     // Retrieve icons from weather API
     var iconCode = response.weather[0].icon;
@@ -88,28 +118,26 @@ function getWeather(city) {
     $("#2dayIcon").attr("src", "http://openweathermap.org/img/w/" + iconCode + ".png");
     $("#3dayIcon").attr("src", "http://openweathermap.org/img/w/" + iconCode + ".png");
 
-   // Display searched city name, temperature, humidity, wind speed, UV index (incl. weather icon)
-
-    // Transfer content to HTML
-    $("#currentCity").text("<h2>" + response.name + "</h2>");
-    $("#temperature").text("Temperature: " + response.main.temp);
-    $("#humidity").text("Humidity: " + response.main.humidity);
-    $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-    $("#uvIndex").text("UV Index: " + response.main.uvi);
-    var lat = response.city.coord.lat;
-    var long = response.city.coord.lon;
-    uvi(lat, long)
-      
-    // Log the data in the console as well
-    console.log("Wind Speed: " + response.wind.speed);
-    console.log("Humidity: " + response.main.humidity);
-    console.log("Temperature: " + response.main.temp);
-    console.log("Temperature: " + response.main.uvi);
     });
-  }
-    //Call searchHistory function when page loads
+
+        // Function to activate click search on recently searched cities stored in search history
+        function storedCities() {
+        // Store cities' name value in $(this)
+        var city = $(this)[0].innerHTML;
+        // When user clicks on stored cities in the search history, city name is transferred
+        getWeather(city);
+    }
+     // On click function for stored cities
+     $(".fas").on("click", getSearchInput);
+     $(document).on("click", ".Search", storedCities);
+
+    // Call searchHistory function when the dashboard page loads
     searchHistory();
-});
+    });
+
+// Function getWeather for current weather and 5 day forecast
+// function getWeather(city) {
+//     console.log(city);
 
 // Given a weather dashboard with form inputs,
 // When user searches for a city, then user is presented with current and future conditions for that city and that city is added to the search history
